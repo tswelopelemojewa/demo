f53458d34b2f9c5f5e8fa6a44b217ec3340a9f04
Tswelopele Mojewa <tswelopelemojewa@gmail.com>
2023-05-27 14:21:45 +0200

    first commit

---
+++ vite.config.js
@@ -0,0 +1,41 @@
+import handlebars from 'vite-plugin-handlebars';
+import { defineConfig } from 'vite'
+
+
+const basicContext = {
+        name: "Tswelopele",
+        email: "tswelopelemojewa@gmail.com",
+        title : "Welcome",
+        show : true,
+        fruits : [
+            "apples",
+            "oranges",
+            "bananas"
+        ],
+        isClient : true,
+        fruitSales : [
+            {day : "Monday", apples : 13, oranges : 34},
+            {day : "Tuesday", apples : 17, oranges : 28},
+            {day : "Thursday", apples : 7, oranges : 45},
+        ]
+    };
+
+
+export default defineConfig({
+  base : '/demo/',
+  build: {
+      rollupOptions: {
+          input : {
+              main: 'index.html',
+              bootstrap: 'index-bootstrap.html',
+              materialize: 'index-materialize.html',
+          }
+      },
+  },
+  plugins: [handlebars({
+    context : {
+      basicContext
+    }
+})]
+  
+});
